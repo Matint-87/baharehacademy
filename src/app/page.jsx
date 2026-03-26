23e45d9b7c2e5427e17f11590fc304dd93c46be7
matin <sajjad.torabi95@gmail.com>
2026-03-26 18:55:35 +0330

    start create log in page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,4 @@
 import { Testimonials } from "../components/Testimonials";
-import { Recipes } from "../components/Recipes";
 import Hero from "../components/Hero";
 import ProductSlider from "../components/ProductSlider";
 import { FaArrowLeft } from "react-icons/fa";
@@ -9,7 +8,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <div className="flex px-5 my-20 flex-col gap-10">
+      {/* <div className="flex px-5 my-20 flex-col gap-10">
         <div className="flex justify-between">
         <h1 className="text-xl font-bold">محصولات جدید</h1>
         <Link href="/products" className="text-sm font-bold flex items-center gap-2 text-[#E24257]">
@@ -20,9 +19,7 @@ export default function Home() {
         <div className="h-70">
           <ProductSlider />
         </div>
-      </div>
-      <Recipes />
-      <Testimonials />
+      </div> */}
     </main>
   );
 }
